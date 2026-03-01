2e6a6d95f31f600df2bb9d6b72d9d78c0a8f62ad
bt4426 <bt4426@srmist.edu.in>
2026-03-01 23:41:17 +0530

    Added UC4 - String Array and Loop Implementation

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,74 +1,23 @@
-/**
- * OOPSBannerApp - UC3: Render OOPS Banner using String.join()
- *
- * This version refactors UC2 implementation by replacing
- * string concatenation (+) with String.join() method.
- *
- * Benefits:
- * - Reduces intermediate String object creation
- * - Improves memory efficiency
- * - Enhances readability
- *
- * @author Tharun
- * @version 3.0
- */
-
 public class OOPSBannerApp {
-
-    // Static method to print OOPS banner
-    public static void printBanner() {
-
-        System.out.println(String.join(" ",
-                " *****  ",
-                " *****  ",
-                " *****  ",
-                " *****  "
-        ));
-
-        System.out.println(String.join(" ",
-                "*     * ",
-                "*     * ",
-                "*     * ",
-                "*     * "
-        ));
-
-        System.out.println(String.join(" ",
-                "*     * ",
-                "*     * ",
-                "*     * ",
-                "*     * "
-        ));
-
-        System.out.println(String.join(" ",
-                "*     * ",
-                "*     * ",
-                "*     * ",
-                " *****  "
-        ));
-
-        System.out.println(String.join(" ",
-                "*     * ",
-                "*     * ",
-                "*     * ",
-                "      * "
-        ));
-
-        System.out.println(String.join(" ",
-                "*     * ",
-                "*     * ",
-                "*     * ",
-                "      * "
-        ));
-
-        System.out.println(String.join(" ",
-                " *****  ",
-                " *****  ",
-                " *****  ",
-                " *****  "
-        ));
-    }
 
     public static void main(String[] args) {
         printBanner();
+    }
+
+    public static void printBanner() {
+
+        // UC4 Implementation using Array and Loop
+        String[] banner = {
+
+            "  OOO     OOO    PPPPP    SSSSS ",
+            " O   O   O   O   P    P  S      ",
+            " O   O   O   O   PPPPP    SSSSS ",
+            " O   O   O   O   P            S ",
+            "  OOO     OOO    P       SSSSS  "
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
